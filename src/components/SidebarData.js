d003655364bcc2d5b817ddefc55bdd70806d3a2d
Robert Bonham <reb159@txstate.edu>
2022-03-04 02:05:23 -0600

    Working SubMenu feature
    All thats left todo is fix the file structure to match what we have prior to this feature

--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -2,7 +2,81 @@ import React from 'react'
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
 import * as IoIcons from "react-icons/io";
+import * as RiIcons from "react-icons/ri";
 
+export const SidebarData = [{
+
+    title: 'Home',
+    path: '/',
+    icon: <AiIcons.AiFillHome />,
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpened: <RiIcons.RiArrowDropUpFill />,
+
+},
+    {
+        title: 'FirstnSecond',
+        path: '/FirstnSecond',
+        icon: <IoIcons.IoIosSchool />,
+        cName: 'nav-text',   
+        iconClosed: <RiIcons.RiArrowDownSFill />,
+        iconOpened: <RiIcons.RiArrowDropUpFill />,
+
+        // where info of the dropdown menu will go 
+        subNav:[
+            {
+                title: 'Game1',
+                path: '/pages/game-pages/GamePage.js',
+                icon: <IoIcons.IoIosSchool />,
+            },
+            // Add addition games here **copy similar layout from subNav**
+        ]
+    },
+    {
+        title: 'ThirdnFourth',
+        path: '/ThirdnFourth',
+        icon: <IoIcons.IoIosSchool />,
+        cName: 'nav-text',   
+        iconClosed: <RiIcons.RiArrowDownSFill />,
+        iconOpened: <RiIcons.RiArrowDropUpFill />,
+        
+        // where info of the dropdown menu will go 
+        subNav:[
+            {
+                title: 'Game1',
+                path: '/pages/game-pages/GamePage.js',
+                icon: <IoIcons.IoIosSchool />,
+            },
+            // Add addition games here **copy similar layout from subNav**
+        ]
+    },
+    {
+        title: 'FifthnSix',
+        path: '/FifthnSix',
+        icon: <IoIcons.IoIosSchool />,
+        cName: 'nav-text',   
+        iconClosed: <RiIcons.RiArrowDownSFill />,
+        iconOpened: <RiIcons.RiArrowDropUpFill />,
+        // where info of the dropdown menu will go 
+        subNav:[
+            {
+                title: 'Game1',
+                path: '/pages/game-pages/GamePage.js',
+                icon: <IoIcons.IoIosSchool />,
+            },
+            // Add addition games here **copy similar layout from subNav**
+        ]
+    },
+    {
+        title: 'LeaderBoards',
+        path: '/pages/LeaderBoard.js',
+        icon: <IoIcons.IoIosSchool />,
+        cName: 'nav-text',   
+
+    }
+]
+
+
+/*
 export const SidebarData = [
     {
         title: 'Home',
@@ -36,3 +110,4 @@ export const SidebarData = [
     },
 
 ]
+*/
