8dc7b3aa8dc0b89dec547b4d66022f4321beab39
Oscar Fortanel <o_f43@txstate.edu>
2022-04-04 00:31:49 -0500

    Implemented logic to submit high scores

--- api/endpoint/includes/verifyRequest.php
+++ api/endpoint/includes/verifyRequest.php
@@ -8,8 +8,37 @@
         }
 
         function verifyPostScore(){
-            if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['api_token']) && $_POST['api_token'] == API_KEY && isset($_POST['name']) && isset($_POST['score'])){
-                return true;
+
+            if($_SERVER['REQUEST_METHOD'] == 'POST'){
+                if(isset($_POST['api_token']) && $_POST['api_token'] == API_KEY && isset($_POST['name']) && isset($_POST['score']) && isset($_POST['g_id'])){
+					if(is_numeric($_POST['score'])){
+                    	return true;
+					}else{
+						return false;
+					}
+                }else{
+                    return false;
+                }
+            }else{
+                echo 'You might have taken a wrong turn!<br> What you\'re looking for isn\'t here.';
+                return false;
+            }
+        }
+
+        function verifyCheckScore(){
+            if($_SERVER['REQUEST_METHOD'] == 'POST'){
+			    if(isset($_POST['api_token']) && $_POST['api_token'] == API_KEY && isset($_POST['score']) && isset($_POST['g_id'])){
+					if(is_numeric($_POST['score'])){
+                    	return true;
+					}else{
+						return false;
+					}
+                }else{
+                    return false;
+                }
+            }else{
+                echo 'You might have taken a wrong turn!<br> What you\'re looking for isn\'t here.';
+                return false;
             }
         }
     }
